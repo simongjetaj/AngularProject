189607c0174b7768c22d5da17084a2fa625df784
Simon Gjetaj <gjetajsimon@gmail.com>
2018-08-01 15:51:24 +0200

    Add pagination to products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
@@ -43,7 +44,8 @@ const appRoutes: Routes = [
     FormsModule,
     HttpModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    NgxPaginationModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
